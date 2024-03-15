f81455134a5f07b14f9a85b8f53ba468abf474ab
itsmeshiwams <itsmeshiwams@gmail.com>
2024-03-15 11:47:48 +0545

    bac

---
+++ first.js
@@ -0,0 +1,104 @@
+
+//console.log("Hello world");
+//variable declaration in js
+//let , var, const
+
+//what are the difference betn Let, var and constant?(interview question)
+
+// var a =10;
+// console.log(a);
+
+// var a =20;
+// console.log(a);
+
+// a=30;
+// console.log(a);
+
+// let b =5;
+// b=15;
+// console.log(b);
+
+// const c = 10;
+// console.log(c);
+
+// variable naming Rules
+// Variable names are case-sensitive ("myVar"is different from "myvar")
+// Variable names can include letters, digits, underscores, and dollar Signs
+// The first character must be a letter, an underscore (_) or a dollar sign($)
+
+
+//camel rule
+//myFirstName
+
+
+// let _firstName = "Shiwams";
+
+// let _firstname = "Shiwams";
+
+
+// data types in js 
+// primitive -> String, Number, Boolean, undefined, null, BigInt,Symbol
+// non primitive-> Array, Object
+
+// let firstName ='10'
+// console.log(typeof firstName) // output --> string
+// let a =20;
+// console.log(typeof a) //output --> number
+
+// let isAbsent = true;
+// console.log(typeof isAbsent ) // output --> boolean
+// console.log(isAbsent);
+
+// let c;
+// console.log(c ); // output --> undefined
+
+//Arithmetic Operators in J-S (+,-,*,/ ,% (modulus operator))
+//  let a = 10;
+//  let b = 30;
+// console.log(a  + b); // output -->40
+// console.log(a -b); // output
+// let c = a + b;
+// console.log(c);
+
+// if (a > b){
+//     console.log("a is greater than b")
+//     }else if (a == b){  
+//         console.log ("a is equal to b");
+//         } else{
+//         console.log("b is  greater than a");
+// }
+//comparasion operator
+// equal  to ==
+
+//create a simple calculator using an arithmetic operator and if else statement
+let a = 10;
+let b = 30;
+let c = "+";
+
+if (op == "+"){
+    console.log(a+b);
+
+}else if
+(op == "-"){
+    console.log(a-b); 
+     
+
+}else if
+(op == "*"){
+    console.log(a*b); 
+     
+
+}else if
+(op == "/"){
+    console.log(a/b); 
+     
+}else if
+(op == "/"){
+    console.log(a/b); 
+     
+}
+else[
+    console.log("Invalid")
+]
+
+
